df73031c12146907112c59b206f975fcec4253b8
Jacob Cantor <jake.cantor@gmail.com>
2014-01-10 18:19:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Universityab::Application.config.secret_key_base = 'f82806116d06099b6cecacdb089e9bd66a0df900a2c516fdb1df984c3ff84e8657d2f6641dd373144522252c40fbf59986baa3c27c79b8e9c03d60c51c1adab7'
